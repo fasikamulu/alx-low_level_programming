dbe68fee6ab69bc92015c8c5ae15fd9539ea9ee8
fasikamulu <fasikamlugetak@gmail.com>
2023-09-14 01:35:29 -0700

    1. I sometimes.......

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,21 @@
+#include "main.h"
 #include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
 
 void print_alphabet(void)
 {
-    char letter = 'a';
+	char c = 'a';
 
-    while (letter <= 'z') {
-        _putchar(letter);
-        letter++;
-    }
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 
-    _putchar('\n');
+	}
+	_putchar('\n');
+
 }
